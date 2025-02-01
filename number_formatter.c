c32fde6b421f8d89532ce358a7020331aa909381
Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2025-02-01 09:29:56 +0600

    note added

--- number_formatter.c
+++ number_formatter.c
@@ -1,15 +1,40 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main() {
+    // Open the file for writing
+    FILE *outputFile = fopen("public/dest.txt", "w");
+    if (outputFile == NULL) {
+        perror("Error opening file");
+        printf("Check if the directory './public' exists and has write permissions.\n");
+        return 1;
+    }
 
+    // Read characters from stdin and write to the file
     int c = fgetc(stdin);
-    while(c!=EOF){
-        fprintf(stdout,"%c",(char)c);
+    /** If we press ctrl + d it will exit from the while loop */
+    while (c != EOF) {
+        fprintf(outputFile, "%c", c);
+        fflush(outputFile);
         c = fgetc(stdin);
     }
 
+    // Close the file
+    fclose(outputFile);
+    printf("Data written to ./public/dest.txt successfully.\n");
 
-
-    exit(0);
+    return 0;
 }
+
+/** End of File
+        EOF = '\0'
+*/
+
+/*
+// To print from stdin
+while(c!='\0'){
+        fprintf(stdout,"%c",(char)c);
+        c = fgetc(stdin);
+}
+
+ */
